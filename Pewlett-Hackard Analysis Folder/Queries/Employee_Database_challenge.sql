b021d4759e6c09324b3956a6d04437601ba278ab
anamahmed15 <86752046+anamahmed15@users.noreply.github.com>
2021-11-02 23:27:02 -0400

    Update Employee_Database_challenge.sql

--- Pewlett-Hackard Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Pewlett-Hackard Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -1,7 +1,5 @@
 DELIVERABLE 1
 
-Employee_Database_challenge.sql
-
 -- Retirement Titles Table - historical titles of those who are 
 -- eligible for retirement
 SELECT  e.emp_no,
@@ -14,8 +12,9 @@ FROM employees as e
 	ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
 ORDER BY e.emp_no, ti.from_date DESC;
-SELECT * FROM retirement_titles
-;
+
+SELECT * FROM retirement_titles;
+
 -- Unique Titles Table - most recent titles of those who are 
 -- eligible for retirement
 SELECT DISTINCT ON (emp_no) emp_no,
@@ -24,8 +23,10 @@ SELECT DISTINCT ON (emp_no) emp_no,
 					title
 INTO unique_titles
 FROM retirement_titles 
-ORDER BY emp_no, from_date DESC
-;
+ORDER BY emp_no, from_date DESC;
+
+SELECT * FROM unique_titles;
+
 -- Retiring Titles Count table - number of people from each title who
 -- are eligible for retirement
 SELECT COUNT (title),
@@ -33,8 +34,10 @@ SELECT COUNT (title),
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
-ORDER BY "count" DESC
-;
+ORDER BY "count" DESC; 
+
+SELECT * FROM retiring_titles;
+
 -- Mentorship Eligibility Table - employees 10 years out from retirement
 -- who are eligible for mentorship program
 SELECT DISTINCT ON (e.emp_no)
@@ -54,3 +57,5 @@ FROM employees AS e
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no, ti.from_date DESC;
+
+SELECT * FROM mentorship_eligibility;
